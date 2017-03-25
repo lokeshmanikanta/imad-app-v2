9f8840ccd2e8ac19eed86e9e9362395d0d928726
lokeshmanikanta <lokeshmanikanta@gmail.com>
2017-03-25 12:50:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone', function(req, res){
-    req.send('Article one requested and will be served here');
+    res.send('Article one requested and will be served here');
 }}
 app.get('/articletwo', function(req, res){
-    req.send('Article two requested and will be served here');
+    res.send('Article two requested and will be served here');
 }}
 app.get('/article', function(req, res){
-    req.send('Article three requested and will be served here');
+    res.send('Article three requested and will be served here');
 }}
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
